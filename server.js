f292f863b6a0fd9f7d731306413754c14a1c936d
sekarSyafira <sekarsyafira225@gmail.com>
2021-07-17 19:29:26 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const notes = require('./src/api/notes');
+const NotesService = require('./src/services/postgres/0penMusicService');
+const NotesValidator = require('./src/validator/notes');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: process.env.PORT,
+		host: process.env.HOST,
+		routes: {
+			cors: {
+				origin: ['*'],
+			},
+		},
+	});
+
+	await server.registe;
+};
