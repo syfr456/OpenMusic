957934ee04f772e888bcc02c8f093812b82cd358
syfr456 <sekarsyafira225@gmail.com>
2021-08-08 00:54:54 +0700

    userplugin in hapi

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-/* eslint-disable no-console */
-/* eslint-disable no-tabs */
+//* eslint-disable no-tabs */
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// songs
 const songs = require('./src/api/songs');
 const SongsService = require('./src/services/postgres/SongService');
 const SongsValidator = require('./src/validator/songs');
 
+// users
+const users = require('./src/api/users');
+const UsersService = require('./src/services/postgres/UsersService');
+const UsersValidator = require('./src/validator/users');
+
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
